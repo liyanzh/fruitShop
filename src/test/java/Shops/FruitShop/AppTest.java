8ae521cbf8ef7710d1d7d37e79dc7773f737abd5
liyanzh <44238274+liyanzh@users.noreply.github.com>
2019-07-03 18:33:32 -0400

    add test cases
    empty cart: mytotal = 0.0, expected = 0.0
    1 apple only: mytotal = 60.0, expected = 60.0
    2 apples only: mytotal = 60.0, expected = 60.0
    3 apples only: mytotal = 120.0, expected = 120.0
    4 apples only: mytotal = 120.0, expected = 120.0
    4 apples plus 1 orange: mytotal = 145.0, expected = 145.0
    4 apples plus 2 orange: mytotal = 170.0, expected = 170.0
    4 apples plus 3 orange: mytotal = 170.0, expected = 170.0

--- src/test/java/Shops/FruitShop/AppTest.java
+++ src/test/java/Shops/FruitShop/AppTest.java
@@ -36,20 +36,68 @@ public class AppTest
      */
     public void testApp()
     {
+    	
     	 ShoppingCart myCart = new ShoppingCart();
          
-         myCart.addItem(Fruit.Apple);
-         myCart.addItem(Fruit.Apple);
-         myCart.addItem(Fruit.Orange);
-         myCart.addItem(Fruit.Apple);
-         myCart.addItem(Fruit.UNKNOWN);
-         myCart.addItem(Fruit.Orange);
-         myCart.addItem(Fruit.Orange);
-         
-         double myTotal = myCart.getTotal();
-         double expectedTotal = Fruit.Apple.getPrice() * 2 + Fruit.Orange.getPrice() * 2;
-         System.out.println("mytotal = " + myTotal + ", expected = " + expectedTotal);
+    	 //test empty shopping cart:
+    	 double myTotal = myCart.getTotal();
+         double expectedTotal = 0;
+         System.out.println("empty cart: mytotal = " + myTotal + ", expected = " + expectedTotal);
          assertEquals(expectedTotal, myTotal); 
-       
+         
+         //test one apple only, the price should equals to one apple instead of half price 
+         myCart.addItem(Fruit.Apple);
+         
+         myTotal = myCart.getTotal();
+         expectedTotal = Fruit.Apple.getPrice() ;
+         System.out.println("1 apple only: mytotal = " + myTotal + ", expected = " + expectedTotal);
+         assertEquals(expectedTotal, myTotal); 
+
+         
+         //test two apples, the price should equals to one apple
+         myCart.addItem(Fruit.Apple);
+         
+         myTotal = myCart.getTotal();
+         expectedTotal = Fruit.Apple.getPrice() ;
+         System.out.println("2 apples only: mytotal = " + myTotal + ", expected = " + expectedTotal);
+         assertEquals(expectedTotal, myTotal); 
+         
+         //test three apples, the price should equals to two apples
+         myCart.addItem(Fruit.Apple);
+         
+         myTotal = myCart.getTotal();
+         expectedTotal = Fruit.Apple.getPrice()*2 ;
+         System.out.println("3 apples only: mytotal = " + myTotal + ", expected = " + expectedTotal);
+         assertEquals(expectedTotal, myTotal); 
+         
+         //test four apples, the price should equals to two apples
+         myCart.addItem(Fruit.Apple);
+         
+         myTotal = myCart.getTotal();
+         expectedTotal = Fruit.Apple.getPrice()*2 ;
+         System.out.println("4 apples only: mytotal = " + myTotal + ", expected = " + expectedTotal);
+         assertEquals(expectedTotal, myTotal); 
+         
+         //test 4 apples and 1 oranges, the total should equals sum of 2 apples plus 1 orange
+         myCart.addItem(Fruit.Orange);
+         myTotal = myCart.getTotal();
+         expectedTotal = Fruit.Apple.getPrice()*2 + Fruit.Orange.getPrice() ;
+         System.out.println("4 apples plus 1 orange: mytotal = " + myTotal + ", expected = " + expectedTotal);
+         assertEquals(expectedTotal, myTotal); 
+         
+         //test 4 apples and 2 oranges, the total should equals sum of 2 apples plus 1 orange
+         myCart.addItem(Fruit.Orange);
+         myTotal = myCart.getTotal();
+         expectedTotal = Fruit.Apple.getPrice()*2 + Fruit.Orange.getPrice() * 2 ;
+         System.out.println("4 apples plus 2 orange: mytotal = " + myTotal + ", expected = " + expectedTotal);
+         assertEquals(expectedTotal, myTotal); 
+         
+         //test 4 apples and 3 oranges, the total should equals sum of 2 apples plus 1 orange
+         myCart.addItem(Fruit.Orange);
+         myTotal = myCart.getTotal();
+         expectedTotal = Fruit.Apple.getPrice()*2 + Fruit.Orange.getPrice() * 2 ;
+         System.out.println("4 apples plus 3 orange: mytotal = " + myTotal + ", expected = " + expectedTotal);
+         assertEquals(expectedTotal, myTotal); 
     }
+    
 }
